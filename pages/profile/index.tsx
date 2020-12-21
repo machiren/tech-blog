ed48a27c07c1462c8d33327b277a58c36e3d64b0
ren machida <r-machida@c3reve.co.jp>
2020-12-21 21:35:36 +0900

    #3 chakra-ui-component導入

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { Button, Container, Heading, useToast } from "@chakra-ui/react"
 
 export async function getStaticProps() {
   // ビルド時刻の取得
@@ -16,23 +17,47 @@ export async function getStaticProps() {
 }
 
 export default function Profile({name, build_time}){
+  const toast = useToast();
   return (
   <>
-    <Head>
-      <title>Profile</title>
-    </Head>
-    <h1>
-      <Link href="/">
-        <a>Back to home</a>
-      </Link>
-    </h1>
-    <h1>
-      <Link href="/ssg-test">
-        <a>Link SSG TEST</a>
-      </Link>
-    </h1>
-    <p>私は {name} です</p>
-    <p>ビルドした日時は {build_time} です</p>
+    <Container>
+      <Head>
+        <title>Profile</title>
+      </Head>
+      <Heading>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </Heading>
+      <Heading>
+        <Link href="/ssg">
+          <a>Link SSG TEST</a>
+        </Link>
+      </Heading>
+      <Heading as="h1">
+        <Link href="/ssr">
+          <a>Link SSR TEST</a>
+        </Link>
+      </Heading>
+      <h1>
+
+      </h1>
+      <p>私は {name} です</p>
+      <p>ビルドした日時は {build_time} です</p>
+      <Button
+        onClick={() =>
+          toast({
+            title: "チャクラUIお試しです",
+            description: "learn chakra ui framework",
+            status: "success",
+            duration: 4000,
+            isClosable: true,
+          })
+        }
+      >
+        トースト！
+      </Button>
+    </Container>
   </>
   )
 }
